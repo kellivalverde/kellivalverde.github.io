1e0edb5c1fba358c4eaa7584700574021fe0b5e4
kellivalverde <paughka@gmail.comls>
2020-03-01 13:11:30 -0500

    updating nav

--- js/my-github-portfolio-test.js
+++ js/my-github-portfolio-test.js
@@ -30,3 +30,12 @@ function showSlides(n) {
     console.log("Dots number: " + slideIndex);
 
 }
+
+function myMenuHideFunction() {
+    var x = document.getElementById("nav");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
