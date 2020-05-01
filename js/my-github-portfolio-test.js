12210da1f5d989b52b8da7887012bf91c93da738
kellivalverde <paughka@gmail.comls>
2020-05-01 13:57:41 -0400

    updated screen shote

--- js/my-github-portfolio-test.js
+++ js/my-github-portfolio-test.js
@@ -9,55 +9,55 @@ function myNavBarFunction() {
 }
 
 
-// ******  IMAGE CAROUSEL  ******
-var slideIndex = 1;
-showSlides(slideIndex);
+// // ******  IMAGE CAROUSEL  ******
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+// // Next/previous controls
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+// // Thumbnail image controls
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = " block";
-    dots[slideIndex - 1].className += " active";
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     var dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) { slideIndex = 1 }
+//     if (n < 1) { slideIndex = slides.length }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex - 1].style.display = " block";
+//     dots[slideIndex - 1].className += " active";
 
-    //test
-    console.log("Dots update: " + dots[slideIndex - 1].className);
-    console.log("Dots number: " + slideIndex);
+//     //test
+//     console.log("Dots update: " + dots[slideIndex - 1].className);
+//     console.log("Dots number: " + slideIndex);
 
-}
+// }
 // ******   MODAL BIO BUTTON  ******
-var confirmButton = document.getElementById('confirm');
-confirmButton.onclick = function() {
-    var userResponse = confirm('My Full Tech Stack: Java • Spring • Hibernate • JPA • JavaScript • MVC • HTML • CSS • Flexbox • Grid • TDD • Agile (Scrum), Object Oriented Programming (OOP) • AJAX • JSON • Restful APIs • Responsive Design / Mobile • Relational Databases • Source Control / Git • Trello / Kanban • Adobe XD • Adobe Photoshop • Adobe Illustrator');
-    var displayContainer = document.getElementById('confirmResponse');
-    var displayMessage = '';
-    if (userResponse) {
-        displayMessage += ';)'
-    } else {
-        displayMessage += ''
-    }
-    displayContainer.innerText = displayMessage;
-    // if "true," user clickerd "Ok." If "false, user clicked "Cancel"
-    console.log(userResponse); 
-}
+// var confirmButton = document.getElementById('confirm');
+// confirmButton.onclick = function() {
+//     var userResponse = confirm('My Full Tech Stack: Java • Spring • Hibernate • JPA • JavaScript • MVC • HTML • CSS • Flexbox • Grid • TDD • Agile (Scrum), Object Oriented Programming (OOP) • AJAX • JSON • Restful APIs • Responsive Design / Mobile • Relational Databases • Source Control / Git • Trello / Kanban • Adobe XD • Adobe Photoshop • Adobe Illustrator');
+//     var displayContainer = document.getElementById('confirmResponse');
+//     var displayMessage = '';
+//     if (userResponse) {
+//         displayMessage += ';)'
+//     } else {
+//         displayMessage += ''
+//     }
+//     displayContainer.innerText = displayMessage;
+//     // if "true," user clickerd "Ok." If "false, user clicked "Cancel"
+//     console.log(userResponse); 
+// }
 
 
 // ******   COLLAPSIBLE DECRIPTION BUTTONS  ******
